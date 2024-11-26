b8a291d2c711ffd00599bdd0fe9caf65039919f3
anisa2472 <anisa.psari24@gmail.com>
2024-11-26 17:29:53 +0700

    Update env variable

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-	apiKey: process.env.SECRET_FIREBASE_API_KEY,
-	authDomain: process.env.SECRET_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.SECRET_FIREBASE_DATABASE_URL,
-	projectId: process.env.SECRET_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.SECRET_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.SECRET_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.SECRET_FIREBASE_APP_ID
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
